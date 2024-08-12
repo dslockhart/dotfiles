e098a22b4ace57d59bd624feb2e1a2003c49f4c8
dslockhart <78314049+dslockhart@users.noreply.github.com>
2024-08-12 15:07:09 +0100

    add treesitter dependencies

--- config/nvim/lua/plugins/configs/treesitter.lua
+++ config/nvim/lua/plugins/configs/treesitter.lua
@@ -1,5 +1,15 @@
 local options = {
-  ensure_installed = { "lua" },
+  ensure_installed = { 
+    "lua",
+    "vimdoc",
+		"luadoc",
+		"vim",
+		"lua",
+		"markdown"
+    },
+
+
+  },
 
   highlight = {
     enable = true,
